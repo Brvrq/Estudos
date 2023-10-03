5c93cf9e1cb2d4973fdc9d5cc5ac03a27a40b01f
Bruno Q. <brunovrq@gmail.com>
2023-10-03 06:42:30 -0300

    Implementing main program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,30 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 	public static void main(String[] args) {
-	
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.println("Price: ");
+		product.price = sc.nextDouble();
+		System.out.println("Quantity: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println("Product: " + product.name + "Value: R$ " + product.price + " Quantity: " + product.quantity);
+		System.out.println("Total value in stock: " + product.totalValueInStock());
+		
+		
+		sc.close();
 		
 	}
 }
